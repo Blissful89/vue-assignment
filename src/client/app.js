6d1365f1c4b1fb57654d287e6f8f8848ac1b7ca9
Marco van Harten <marcovanharten89@gmail.com>
2021-11-06 10:14:22 +0100

    feat: add primevue for simplicity

--- src/client/app.js
+++ src/client/app.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+
+// Note: Doing this for faster development
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'                           //icons
+import 'primeflex/primeflex.css';                            //flex
 
 const app = createApp(App)
 
-app.mount('#app')
+app.use(PrimeVue).mount('#app')
