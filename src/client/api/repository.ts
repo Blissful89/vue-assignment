e3dfea35183ba55289d46214d880b32f933d8d31
Marco van Harten <marcovanharten89@gmail.com>
2021-11-07 09:44:52 +0100

    refactor: add config to frontend

--- src/client/api/repository.ts
+++ src/client/api/repository.ts
@@ -1,25 +1,24 @@
 import { ref } from 'vue'
 import eventbus from '@/client/utils/eventbus'
-
-const URL = 'ws://localhost:3000'
-const DELAY = 2000
+import config from '@/client/utils/config'
 
 /**
  * This repository opens the connection for vehicle data.
  * It fakes a loading state to mimic real delay
  */
 class Repository {
+  private url = `ws://${config.server.host}:${config.server.port}`
   socket?: WebSocket
   loading = ref(false)
 
   openWebsocket(cb?: any) {
     this.loading.value = true
     setTimeout(() => {
-      this.socket = new WebSocket(URL)
+      this.socket = new WebSocket(this.url)
       this.socket.onopen = cb
       this.socket.onmessage = (e: any) => eventbus.emit('message', e)
       this.loading.value = false
-    }, DELAY)
+    }, 2000)
   }
 
   closeWebsocket(cb?: any) {
---
+++ src/client/utils/config.ts
@@ -0,0 +1,3 @@
+const NODE_ENV = process.env.NODE_ENV
+
+export default require(`../../../config/${NODE_ENV}.json`)
